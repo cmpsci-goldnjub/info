1bf102616b92205a205bb979323f12d4bc974a7e
Michael Wisely <michaelwisely@gmail.com>
2015-08-31 11:07:20 -0500

    Cleanup upload script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,17 @@
-from fabric.api import local, put, sudo
+from fabric.api import local, put, run, sudo
+
+NGINX_DIR = "/usr/share/nginx/www"
 
 def upload():
+    # Build the site
     local('jekyll build')
-    put('_site', '/tmp')
-    sudo("cp -R /tmp/_site/* /usr/share/nginx/www/")
+
+    # Upload it to a temporary place
+    temp_dir = run('mktemp -d')
+    put('_site/*', temp_dir)
+
+    # Overwrite the nginx directory with the new files
+    sudo("rsync --delete -rvc {}/ {}/".format(temp_dir, NGINX_DIR))
+
+    # Cleanup
+    run('rm -rf {}'.format(temp_dir))
